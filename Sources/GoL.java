2b786570ca3941e4aab97bf010fc013ce01c2b64
notKamui <46132319+notKamui@users.noreply.github.com>
2018-12-27 00:55:19 +0100

    Sourcecode

---
+++ Sources/GoL.java
@@ -0,0 +1,191 @@
+
+/*
+ * Java implementation of the Game of Life. Cf doc CGOL.pdf
+ */
+
+import java.util.*;
+
+public class GoL {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int width, height, time, gen = 0;
+		double rate;
+		boolean[][] world;
+		boolean infinite = true;
+
+		boolean importedWorld = true;
+		String fname = "";
+		try {
+			fname = args[0];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			importedWorld = false;
+		}
+
+		int menu;
+		do {
+			GoLFunc.clear();
+			System.out.print("----- The Game of Life -----\n\n" 
+					+ "Please open your terminal in full screen\n\n"
+					+ "1) Generate a full random world\n" 
+					+ "2) Inject a premade world\n" 
+					+ "3) Infos\n" 
+					+ "4) Quit\n\n"
+					+ "What do you want (enter a number) ? ");
+			try {
+				menu = Integer.parseInt(input.nextLine());
+			} catch (NumberFormatException e) {
+				menu = 0;
+			}
+			switch (menu) {
+
+			case 1:
+
+				// choose dimensions
+				do {
+					GoLFunc.clear();
+					System.out.println("Choose the size of the world (recommended maximum dimensions : 200x60) : ");
+					try {
+						System.out.print("Width : ");
+						width = Integer.parseInt(input.nextLine());
+						System.out.print("Height : ");
+						height = Integer.parseInt(input.nextLine());
+						System.out.println();
+					} catch (NumberFormatException e) {
+						width = 0;
+						height = 0;
+					}
+				} while (width <= 0 || height <= 0);
+
+				// choose liferate
+				do {
+					try {
+						GoLFunc.clear();
+						System.out.print("Choose the life-rate of this world (%) (recommended : 10~20) : ");
+						rate = Double.parseDouble(input.nextLine());
+						System.out.println();
+					} catch (NumberFormatException e) {
+						rate = -1;
+					}
+				} while (rate < 0 || rate > 100);
+
+				// choose speed
+				do {
+					try {
+						GoLFunc.clear();
+						System.out
+								.print("Choose the speed of evolution (ms) (minimum : 10 ; recommended : 100~1000) : ");
+						time = Integer.parseInt(input.nextLine());
+						System.out.println();
+					} catch (NumberFormatException e) {
+						time = 0;
+					}
+				} while (time < 10);
+
+				world = GoLFunc.createWorld(width, height);
+
+				GoLFunc.randomLifeFill(world, rate);
+				GoLFunc.displayWorld(world);
+
+				System.out.println();
+				while (infinite) { // infinite loop, only way to get out is CTRL+C
+					world = GoLFunc.nextGenWorld(world);
+					GoLFunc.sleep(time);
+					GoLFunc.displayWorld(world);
+					System.out.print("Generation n°" + gen);
+					gen++;
+				}
+				break;
+
+			case 2:
+				GoLFunc.clear();
+
+				if (!importedWorld) {
+					System.out.println("Not available : no valid argument or file found.");
+					GoLFunc.sleep(2000);
+				} else {
+					// choose dimensions
+					do {
+						try {
+							GoLFunc.clear();
+							System.out.println("Input the EXACT dimensions of the chosen world : ");
+							System.out.print("Width : ");
+							width = Integer.parseInt(input.nextLine());
+							System.out.print("Height : ");
+							height = Integer.parseInt(input.nextLine());
+							System.out.println();
+						} catch (NumberFormatException e) {
+							width = 0;
+							height = 0;
+						}
+					} while (height <= 0 || width <= 0);
+
+					// choose speed
+					do {
+						try {
+							GoLFunc.clear();
+							System.out.print(
+									"Choose the speed of evolution (ms) (minimum : 10 ; recommended : 100~1000) : ");
+							time = Integer.parseInt(input.nextLine());
+							System.out.println();
+						} catch (NumberFormatException e) {
+							time = 0;
+						}
+					} while (time < 10);
+
+					world = GoLFunc.createWorld(width, height);
+
+					GoLFunc.readWorld(world, fname);
+					GoLFunc.displayWorld(world);
+
+					System.out.println();
+					while (infinite) { // infinite loop, only way to get out is CTRL+C
+						world = GoLFunc.nextGenWorld(world);
+						GoLFunc.sleep(time);
+						GoLFunc.displayWorld(world);
+						System.out.print("Generation n°" + gen);
+						gen++;
+					}
+				}
+				break;
+
+			case 3:
+				GoLFunc.clear();
+				System.out.println("----- Infos -----\n"
+						+ "Only follow each step without trying to corrupt the game by entering absurd inputs\n"
+						+ "Please use this in a terminal/console in fullscreen on a Unix system with a recommended font size of 10px.\n"
+						+ "The only way to get out of the game after being lauched is to forcequit it by pressing CTRL+C\n"
+						+ "\n"
+						+ "To run the game, open a terminal/console and move to the current folder using cd commands, then\n"
+						+ "java -jar GoL.jar\n\n"
+						+ "To import a world, create a .txt file and call it via\n"
+						+ "java -jar GoL.jar [absolutePathOfYourTextFile]\n"
+						+ "(you can drag and drop the file in the terminal to get the path)\n"
+						+ "Make sure to only use these characters in it : living : ⬜ ; dead : ⬛\n"
+						+ "Make sure that the world is perfectly rectangle\n"
+						+ "Any error in the text file will cause the game to crash\n"
+						+ "You can use the sample worlds as you wish. \n\n"
+						+ "For further informations, check out GoL.pdf\n"
+						+ "Implemented by notKamui -- December 2018\n" 
+						+ "Inspired by Conway's Game of Life"
+						+ "\n\nPress Enter");
+				String s = input.nextLine();
+				s = s + "";
+				break;
+
+			default:
+				GoLFunc.clear();
+				break;
+
+			}
+
+		} while (menu != 4);
+
+		System.out.println("Bye !");
+		GoLFunc.sleep(2000);
+		GoLFunc.clear();
+
+		input.close();
+	}
+
+}
---
+++ Sources/GoLFunc.java
@@ -0,0 +1,151 @@
+
+/*
+ * Functions for the GoL
+ */
+
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class GoLFunc {
+
+	public static boolean[][] createWorld(int width, int height) {
+		boolean[][] world = new boolean[width][height];
+		deathFill(world);
+		return world;
+	}
+
+	public static void deathFill(boolean[][] world) {
+		int width = world.length;
+		int height = world[0].length;
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
+				world[x][y] = false; // false = non-living cell
+	}
+
+	public static boolean getRandomBool() {
+		return Math.random() < 0.5;
+	}
+
+	public static void randomLifeFill(boolean[][] world, double rate) {
+		// rate represents how many living cells there should be (in percents)
+		int width = world.length;
+		int height = world[0].length;
+		int neededLife = (int) (width * height * (rate / 100));
+
+		while (neededLife > 0) {
+			int x = (int) (Math.random() * width);
+			int y = (int) (Math.random() * height);
+
+			if (!world[x][y]) {
+				world[x][y] = true;
+				if (world[x][y])
+					neededLife--;
+			}
+
+		}
+
+	}
+	
+	public static void readWorld (boolean[][] world, String fname) {
+		int width = world.length;
+		int height = world[0].length;
+		
+		try {
+			Scanner sc = new Scanner(new File(fname));
+			String line;
+			while (sc.hasNextLine()) {
+				for (int y = 0; y < height; y++) {
+					line = sc.nextLine();
+					for (int x = 0; x < width; x++) {
+						if (line.charAt(x) == '⬜')
+							world[x][y] = true;
+					}
+				}
+			}
+			sc.close();
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	// living : ⬜ dead : ⬛
+	public static void displayWorld(boolean[][] world) {
+		int width = world.length;
+		int height = world[0].length;
+
+		clear();
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (world[x][y])
+					System.out.print("⬜");
+				else
+					System.out.print("⬛");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static int livingNeighbors(boolean[][] world, int x, int y) {
+		int width = world.length;
+		int height = world[0].length;
+		int count = 0;
+
+		if (world[Math.floorMod(x - 1, width)][Math.floorMod(y - 1, height)])
+			count++; // top left
+		if (world[Math.floorMod(x, width)][Math.floorMod(y - 1, height)])
+			count++; // top
+		if (world[Math.floorMod(x + 1, width)][Math.floorMod(y - 1, height)])
+			count++; // top right
+		if (world[Math.floorMod(x + 1, width)][Math.floorMod(y, height)])
+			count++; // right
+		if (world[Math.floorMod(x + 1, width)][Math.floorMod(y + 1, height)])
+			count++; // bottom right
+		if (world[Math.floorMod(x, width)][Math.floorMod(y + 1, height)])
+			count++; // bottom
+		if (world[Math.floorMod(x - 1, width)][Math.floorMod(y + 1, height)])
+			count++; // bottom left
+		if (world[Math.floorMod(x - 1, width)][Math.floorMod(y, height)])
+			count++; // left
+
+		return count;
+	}
+
+	public static boolean[][] nextGenWorld(boolean[][] world) {
+		int width = world.length;
+		int height = world[0].length;
+		boolean[][] nextGen = GoLFunc.createWorld(width, height);
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (world[x][y]) { // case living cell
+					if (livingNeighbors(world, x, y) == 2 || livingNeighbors(world, x, y) == 3)
+						nextGen[x][y] = true;
+				} else { // case dead cell
+					if (livingNeighbors(world, x, y) == 3)
+						nextGen[x][y] = true;
+				}
+			}
+		}
+
+		return nextGen;
+
+	}
+
+	public static void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	public static void clear() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+		System.out.println();
+	}
+
+}
